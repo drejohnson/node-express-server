96399535813909677c7150bfec26283faa380f54
DeAndre Johnson <dre@phreshr.com>
2015-08-31 13:27:35 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var morgan = require('morgan');
+var compression = require('compression');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var cookieParser = require('cookie-parser');
+var errorHandler = require('errorhandler');
+var app = express();
+
+app.set('port', (process.env.PORT || 8000));
+
+var staticOptions = {
+    dotfiles: 'ignore',
+    etag: true,
+    extensions: ['htm', 'html'],
+    index: 'index.html',
+    lastModified:true,
+    maxAge: '1d',
+    setHeaders: function (res, path, stat) {
+      res.set('x-timestamp', Date.now());
+      res.header('Cache-Control', 'public, max-age=1d');
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  }
+}
+
+app.use(compression());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(cookieParser());
+app.use(express.static(__dirname + '/public'))
+
+app.use('/', express.static('public', staticOptions ));
+
+app.get('/hello-world', function(request, response) {
+  response.send('Hello World!');
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
