854b554d1dba6d1c2a4933897241b07041832275
DeAndre Johnson <dre@phreshr.com>
2015-09-01 15:33:57 -0400

    Added Redis cache and testing serve-favicon

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var express = require('express');
+var favicon = require('serve-favicon');
 var morgan = require('morgan');
 var compression = require('compression');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var cookieParser = require('cookie-parser');
 var errorHandler = require('errorhandler');
+var helmet = require('helmet');
+
+var redis = require('redis');
+var client = redis.createClient(); //creates a new client
+
 var app = express();
 
 app.set('port', (process.env.PORT || 8000));
@@ -19,12 +25,12 @@ var staticOptions = {
     setHeaders: function (res, path, stat) {
       res.set('x-timestamp', Date.now());
       res.header('Cache-Control', 'public, max-age=1d');
-      res.header("Access-Control-Allow-Origin", "*");
+      res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   }
 }
-
+app.use(helmet());
 app.use(compression());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -33,11 +39,16 @@ app.use(cookieParser());
 app.use(express.static(__dirname + '/public'))
 
 app.use('/', express.static('public', staticOptions ));
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
+client.on('connect', function() {
+  console.log('Redis is connected');
+});
 
 app.get('/hello-world', function(request, response) {
   response.send('Hello World!');
 })
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+  console.log('Node app is running at localhost:' + app.get('port'));
 });
